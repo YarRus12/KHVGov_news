ba551b603d8c272aa5325ebec739e093cacd86d8
Yaroslav Russu <yaroslav.russu@mail.ru>
2022-07-09 09:27:31 +1000

    Задача выполнена в 2 вариантах. Сделан тяжелый краулер для сбора всех ссылок с сайта через Beautiful soup и легкий скраппер для решения такой же задачи строковыми методами и регулярками. Итоговый распарсинг выполняется Beautiful soup

--- Easy_way.py
+++ Easy_way.py
@@ -15,9 +15,9 @@ import re
 
 """pip install -r requirements.txt"""
 
-""" В текущий момент это очень тяжелый краулер, не предназначенный для скоростного поиска.
-Он страшно жрет ресурсы оперативной памати, но при этом может уверено и методично переработать хоть весь сайт Правительства края, 
-лищь бы хватило ресурсов у машины. Его не пустят в ПРОМ"""
+"""Это очень легкий и достаточно быстрый скраппер. Он вытаскивает всю html разметку
+Находим блок инфомрации между заданной датой и предыдущим тегом о дате
+Ищет все вхождения ссылок и распарсевает их"""
 
 URL = 'https://www.khabkrai.ru/events/news' #В качестве URL передаем ссылку на страницу новостей Правительства края
 user_agent = user_agent("chrome")
@@ -53,43 +53,53 @@ def page_open(url: str, headers: str,  iterations: int = 1):
         html_content = driver.page_source
         return html_content
 
-def parse_pages(url: str, headers: str,  iterations: int) -> list:
-    """Функция принимает в себя адрес и заголовки
-    на странице ссылки и возвращает список ссылок"""
-    Links = []
-    html_content = page_open(url, headers, iterations)
-    soup = BeautifulSoup(html_content, 'lxml')
-    #регулярным выражением ищем все ссылки в html и собираем их по сайту
-    for link in soup.find_all('a', href=re.compile('^(/|.*' + url + '/[0-9])')):
-        if link.attrs['href'] is not None:
-
-            if link.attrs['href'] not in Links:
-                if link.attrs['href'].startswith('/'):
-                    Links.append(url + link.attrs['href'])
-                else:
-                    Links.append(link.attrs['href'])
-        else:
-            print("Connection Error")
-    print(f"Обнаружено {len(Links)} ссылок")
-    return list(set(Links))
-
 def parse_content(content: str):
     """Функция принимает в себя контент достает ссылки"""
     #регулярным выражением ищем все ссылки в html и собираем их по сайту
     list_of_links = []
-    links = content.find_all(re.compile('\/events\/news\/[0-9]*')) # нужно сделать это выражение не жадным
-    print(links)
+    links = re.findall('/events/news/[0-9]*', html_content)
+    for link in links:
+        list_of_links.append('https://www.khabkrai.ru'+link)
+    return list_of_links
 
+def news_parser(link):
+    """Функция принимает в себя ссылку, извлекает id, ищет и преобразует дату, ищет заголовок и текст
+    и Возвращает кортеж из указанных значений"""
+    options = webdriver.ChromeOptions()
+    options.add_argument('headless')
+    driver = webdriver.Chrome(chrome_options=options)
+    driver.get(link)
+    try:
+        *args, id = link.split('/')
+        date = driver.find_element(By.CLASS_NAME, 'data-item').text
+        date_l = functional.change_date(date)
+        # заголовок ищется двумя методами
+        try:
+            head = driver.find_element(By.CSS_SELECTOR, 'body > div.main-wrapper > div.content.main.standart_shablons__news_detail > div > div > div.content-block > h1 > span').text
+        except:
+            try:
+                head = driver.find_element(By.XPATH, '/html/body/div[2]/div[3]/div/div/h1/text()').text
+            except: head = 'Заголовок не найден'
+        try:
+            #page_text = driver.find_element(BY.CLASS_NAME, 'content-text').text
+            page_text = driver.find_element(By.XPATH, '/html/body/div[2]/div[3]/div/div/div[2]/div[4]').text # Это самый нестабильный способ, но только он сработал
+        except: page_text = 'На странице не обнаружен текст'
+    except Exception as e: print(e)
+    print(f'Страница распарсена')
+    return id, date_l, head, page_text
 
 year,month,day = str(date.today()).split("-")
 today = f'{day}.{month}.{year}'
 print(f'Сегодня {today}')
-html_content = page_open(URL, headers, 3)
-today = '08.07.2022'
+html_content = page_open(URL, headers, 1)
+#today = '08.07.2022'
 
-begin_index = html_content.find('date-splitter')
+begin_index = html_content.find(today)
 end_index = html_content.find('date-splitter', begin_index+1)
 html_content = html_content[begin_index : end_index]
-print(parse_content(html_content))
+list_of_links = set(parse_content(html_content))
+print(f'В рассматриваемом разделе обнаружены {len(list_of_links)} новостных ссылок')
+result = [news_parser(link) for link in list_of_links] # Ограничимся ВЫБОРКОЙ В 10 СТРАНИЦ
 
-
+for row in result:
+    print(row)
